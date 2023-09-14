3039c92a9bf482187b0927e0d03fc89fdd818a10
Ashish Kashyap <113720626+BlackPhosphorus@users.noreply.github.com>
2023-09-13 23:00:29 -0500

    Updated project under new Android project because Design View broke. Final project is named "FinalFinalCalculatorAppAshishK".

--- CalculatorAppAshish/app/src/main/java/com/example/calculatorappashish/MainActivity.java
+++ CalculatorAppAshish/app/src/main/java/com/example/calculatorappashish/MainActivity.java
@@ -10,84 +10,93 @@ import android.view.View;
 import android.widget.TextView;
 
 import com.google.android.material.button.MaterialButton;
-import org.mozilla.javascript.Context;
-import org.mozilla.javascript.Scriptable;
 
-
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
-
-    TextView result,solution;
-        MaterialButton buttonDivide,buttonMultiply,buttonPlus,buttonMinus,buttonEquals;
-    MaterialButton button0,button1,button2,button3,button4,button5,button6,button7,button8,button9;
-    MaterialButton buttonAC;
-
-    void assignId(MaterialButton btn,int id){
-        btn = findViewById(id);
-        btn.setOnClickListener(this);
-    }
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        result = findViewById(R.id.solution);
-        solution = findViewById(R.id.ongoing);
-        assignId(buttonDivide,R.id.button_divide);
-        assignId(buttonMultiply,R.id.button_multiply);
-        assignId(buttonPlus,R.id.button_plus);
-        assignId(buttonMinus,R.id.button_minus);
-        assignId(buttonEquals,R.id.button_equals);
-        assignId(button0,R.id.button_0);
-        assignId(button1,R.id.button_1);
-        assignId(button2,R.id.button_2);
-        assignId(button3,R.id.button_3);
-        assignId(button4,R.id.button_4);
-        assignId(button5,R.id.button_5);
-        assignId(button6,R.id.button_6);
-        assignId(button7,R.id.button_7);
-        assignId(button8,R.id.button_8);
-        assignId(button9,R.id.button_9);
-        assignId(buttonAC,R.id.button_ac);
-    }
-
-
-    @Override
-    public void onClick(View view) {
-        MaterialButton button =(MaterialButton) view;
-        String buttonText = button.getText().toString();
-        String dataToCalculate = solution.getText().toString();
-
-        if(buttonText.equals("AC")){
-            solution.setText("");
-            result.setText("0");
-            return;
+    public class MainActivity extends AppCompatActivity {
+        TextView solution;
+        private String view2 = "";
+        private double result = 0.0;
+        void assignId(int id) {
+            MaterialButton btn = findViewById(id);
+            btn.setOnClickListener(this::onClick);
         }
-        if(buttonText.equals("=")){
-            solution.setText(result.getText());
-            return;
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
+            solution = findViewById(R.id.ongoing);
+            assignId(R.id.button_divide);
+            assignId(R.id.button_multiply);
+            assignId(R.id.button_plus);
+            assignId(R.id.button_minus);
+            assignId(R.id.button_equals);
+            assignId(R.id.button_0);
+            assignId(R.id.button_1);
+            assignId(R.id.button_2);
+            assignId(R.id.button_3);
+            assignId(R.id.button_4);
+            assignId(R.id.button_5);
+            assignId(R.id.button_6);
+            assignId(R.id.button_7);
+            assignId(R.id.button_8);
+            assignId(R.id.button_9);
+            assignId(R.id.button_ac);
         }
-        solution.setText(dataToCalculate);
 
-        String finalResult = getResult(dataToCalculate);
-
-        if(!finalResult.equals("Err")){
-            result.setText(finalResult);
-        }
-    }
-
-    String getResult(String data){
-        try{
-            Context context  = Context.enter();
-            context.setOptimizationLevel(-1);
-            Scriptable scriptable = context.initStandardObjects();
-            String finalResult =  context.evaluateString(scriptable,data,"Javascript",1,null).toString();
-            if(finalResult.endsWith(".0")){
-                finalResult = finalResult.replace(".0","");
+        public void onClick(View view) {
+            MaterialButton button = (MaterialButton) view;
+            String buttonText = button.getText().toString();
+            if (buttonText.equals("AC")) {
+                view2 = "";
+                result = 0.0;
+                solution.setText(view2);
+                return;
             }
-            return finalResult;
+            view2 += buttonText;
+            solution.setText(view2);
+            if (buttonText.equals("=")){
+                double answer = equals(view2);
+                solution.setText(""+ answer);
+                view2 = "" + answer;
+            }
         }
-        catch (Exception e){
-            return "Err";
+        public double equals(String string){
+            String operator = "";
+            double num1 = 0.0;
+            double num2 = 0.0;
+            int index = 0;
+            if(string.contains("+")) {
+                index = string.indexOf("+");
+                operator = "+";
+            }
+            else if(string.contains("-")) {
+                index = string.indexOf("-");
+                operator = "-";
+            }
+            else if(string.contains("*")) {
+                index = string.indexOf("*");
+                operator = "*";
+            }
+            else if(string.contains("/")) {
+                index = string.indexOf("/");
+                operator = "/";
+            }
+            num1 = parseDouble(string.substring(0, index));
+            num2 = parseDouble(string.substring(index+1, string.length()-1));
+
+            if (operator.equals("+")) {
+                result = num1+num2;
+            }
+            else if (operator.equals("-")) {
+                result = num1-num2;
+            }
+            else if (operator.equals("*")) {
+                result = num1*num2;
+            }
+            else if (operator.equals("/")) {
+                result = num1/num2;
+            }
+            return result;
         }
     }
 }
